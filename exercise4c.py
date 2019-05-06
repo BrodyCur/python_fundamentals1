f1096e852ce162a4b46c37e64c37cc73b048e199
Brody Currie <brodycurrie@gmail.com>
2019-05-06 14:11:09 -0400

    Completed exercise 4c (3)

---
+++ exercise4c.py
@@ -0,0 +1,9 @@
+my_name = "Brody"
+
+print("Enter your first name.")
+your_name = input()
+
+if your_name == my_name:
+    print("My goodness, we have the same name!")
+else:
+    print("Cool name.")
